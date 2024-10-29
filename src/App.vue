7face7f9821e18d233c6106e89424dece938e8bb
Adrien MIQUEL <admiquel94@gmail.com>
2024-10-29 22:56:13 +0100

    Clean folder structure + remove unnecessary files

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div class="bg-yellow-300">
     <header>
-      <img
-        alt="Vue logo"
-        class="logo"
-        src="@/assets/logo.svg"
-        width="125"
-        height="125"
-      />
-
       <div class="wrapper">
         <HelloWorld :msg="$t('appName')" />
 
@@ -32,11 +23,6 @@ import HelloWorld from './components/HelloWorld.vue'
 header {
   line-height: 1.5;
   max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 nav {
@@ -69,10 +55,6 @@ nav a:first-of-type {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
---
+++ src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h3>Hello World!</h3>
+  </main>
+</template>
