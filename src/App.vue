043f63d3d99722e16bd9d0f1d863107d97b10fd2
Adrien MIQUEL <admiquel94@gmail.com>
2024-10-29 23:06:32 +0100

    Create a dedicated Header component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/layout/Header.vue'
 import Footer from './components/layout/Footer.vue'
 </script>
 
 <template>
   <div class="bg-yellow-300">
-    <header>
-      <div class="wrapper">
-        <HelloWorld :msg="$t('appName')" />
-
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div>
-    </header>
-
+    <Header />
     <RouterView />
   </div>
   <Footer />
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="absolute top-[20px] left-[20px]">
+    <h1 class="text-lg uppercase">Header</h1>
+  </header>
+</template>
