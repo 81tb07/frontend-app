abcfe832119b08ad9beaa33600fafa81092115c2
Adrien Miquel <47077258+AvocadoVenom@users.noreply.github.com>
2024-10-30 17:04:01 +0100

    chore(i18n): Add i18n library to manage translations (#1)
    * Add vue-i18n
    
    * Init locale files
    
    * Set up i18n lib
    
    * Use translations

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { createI18n } from 'vue-i18n'
+import en from './locales/en.json'
+import fr from './locales/fr.json'
+
+const i18n = createI18n({
+  locale: "fr",
+  fallbackLocale: "en",
+  messages: { en, fr }
+})
+
 const app = createApp(App)
 
 app.use(router)
 
+app.use(i18n)
 app.mount('#app')
