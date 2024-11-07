e181404fbd8fd1ee3fb52976abe3e5d6f7ced012
Adrien MIQUEL <admiquel94@gmail.com>
2024-11-07 23:33:34 +0100

    WIP

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,10 +2,22 @@
 
 <template>
   <main
-    class="w-screen h-screen m-auto flex flex-col items-center justify-center"
+    class="w-screen max-w-[600px] mx-auto h-screen px-8 flex flex-col items-start gap-6 content-stretch justify-center"
   >
-    <p class="text-base">
-      Here, we will hold the main content of the login page
-    </p>
+    <h1 class="text-3xl">{{ $t('loginPage.title') }}</h1>
+    <p class="text-lg" v-html="$t('loginPage.headline')" />
+    <div class="flex items-center justify-between w-full"></div>
+    <div class="flex flex-col px-6 w-full gap-3">
+      <RouterLink
+        class="block text-center py-2 w-full rounded-2xl bg-amber-500 shadow-2xl"
+        to="/login"
+        >{{ $t('loginPage.form.ctas.submit') }}</RouterLink
+      >
+      <RouterLink
+        class="block text-center py-2 w-full rounded-2xl shadow-2xl border border-solid border-4 border-slate-900"
+        to="/"
+        >{{ $t('loginPage.form.ctas.cancel') }}</RouterLink
+      >
+    </div>
   </main>
 </template>
