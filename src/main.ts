f0a5f43deaa63baa88b67eddf394485cf32eb728
Adrien MIQUEL <admiquel94@gmail.com>
2024-11-15 00:25:29 +0100

    Extract i18n setup into dedicated file

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,8 @@ import './assets/styles/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { i18n } from './locales/i18n'
 
-import { createI18n } from 'vue-i18n'
-import en from './locales/en.json'
-import fr from './locales/fr.json'
-
-const i18n = createI18n({
-  locale: "fr",
-  fallbackLocale: "en",
-  messages: { en, fr }
-})
 
 const app = createApp(App)
 
