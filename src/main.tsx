8d7d03d5d62d17ea8ad550c864ef43357a8a5c6e
Adrien MIQUEL <admiquel94@gmail.com>
2024-11-27 13:54:23 +0100

    Reintroduce i18n

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RoutesContainer } from './routing/RoutesContainer.tsx';
 
+import './locales/i18n.ts'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RoutesContainer />
